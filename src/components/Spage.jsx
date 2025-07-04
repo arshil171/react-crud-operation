dec067193a66ee11b688fca542cefb0cf5ff1a32
HappyOP411 <harpalkhachar9@gmail.com>
2025-07-04 16:00:08 +0530

    crud

--- src/components/Spage.jsx
+++ src/components/Spage.jsx
@@ -1,51 +1,93 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
-import "../App.css"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import "../App.css";
 
 const Spage = () => {
+  let [product, setProduct] = useState({
+    urlmain: ".",
+    url1: "",
+    url2: " ",
+    brand: "",
+    title: "",
+    price: "",
+  });
+  let [image, setImage] = useState("");
 
-  let [product, setProduct] = useState({ urlmain: ".", url1: "", url2: " ", brand: "", title: "", price: "" })
+  const { id } = useParams();
 
-
-  const { id } = useParams()
-
+  function handleimage(src) {
+    console.log(src);
+    setImage(src);
+  }
 
   async function getData() {
     try {
       let res = await axios.get(`http://localhost:3000/products/${id}`);
       console.log(res.data);
-      setProduct(res.data)
-
+      setProduct(res.data);
     } catch (error) {
       console.log(error);
     }
   }
-
 
   useEffect(() => {
     getData();
   }, []);
 
   return (
-
     <div>
-
-      <section  className="section-p1 "  id="prodetails">
+      <section className="section-p1 " id="prodetails">
         <div className="single-pro-image">
-          <img src={"." + product.urlmain || null} alt="" />
+          <img src={image ? "." + image : "." + product.urlmain} alt="" />
           <div className="small-img-group">
-            <div className="small-img-col">
-              <img src={"." + product.urlmain || null} width="100%" className="small-img" />
+            <div
+              onClick={() => {
+                handleimage(product.urlmain);
+              }}
+              className="small-img-col"
+            >
+              <img
+                src={"." + product.urlmain || null}
+                width="100%"
+                className="small-img"
+              />
             </div>
-            <div className="small-img-col">
-              <img src={"." + product.url1 || null} width="100%" className="small-img" />
+            <div
+              onClick={() => {
+                handleimage(product.url1);
+              }}
+              className="small-img-col"
+            >
+              <img
+                src={"." + product.url1 || null}
+                width="100%"
+                className="small-img"
+              />
             </div>
-            <div className="small-img-col">
-              <img src={"." + product.url2 || null} width="100%" className="small-img" />
+            <div
+              onClick={() => {
+                handleimage(product.url2);
+              }}
+              className="small-img-col"
+            >
+              <img
+                src={"." + product.url2 || null}
+                width="100%"
+                className="small-img"
+              />
             </div>
-            <div className="small-img-col">
-              <img src={"." + product.url3 || null} width="100%" className="small-img" />
+            <div
+              onClick={() => {
+                handleimage(product.url3);
+              }}
+              className="small-img-col"
+            >
+              <img
+                src={"." + product.url3 || null}
+                width="100%"
+                className="small-img"
+              />
             </div>
           </div>
         </div>
@@ -54,56 +96,29 @@ const Spage = () => {
           <h6>Home / T-Shirt</h6>
           <h4>{product.title}</h4>
           <h2>{product.price}</h2>
-          <select >
-            <option >Select Size</option>
-            <option >XL</option>
-            <option >XXL</option>
-            <option >Small</option>
-            <option >Large</option>
+          <select>
+            <option>Select Size</option>
+            <option>XL</option>
+            <option>XXL</option>
+            <option>Small</option>
+            <option>Large</option>
           </select>
           <input type="number" value={1} />
-             <button className="normal">Add To Cart</button>
-              <h4>Product Details</h4>
-               <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta culpa impedit omnis! Commodi soluta beatae facilis omnis placeat unde, sed accusamus temporibus alias, impedit laboriosam odit veritatis voluptatem ad corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sunt, magni eos ratione repellendus atque neque corrupti magnam, natus quis illum harum consectetur. Quasi suscipit nam cum quo quia vero!</span>
+          <button className="normal">Add To Cart</button>
+          <h4>Product Details</h4>
+          <span>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+            culpa impedit omnis! Commodi soluta beatae facilis omnis placeat
+            unde, sed accusamus temporibus alias, impedit laboriosam odit
+            veritatis voluptatem ad corrupti. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Quidem sunt, magni eos ratione
+            repellendus atque neque corrupti magnam, natus quis illum harum
+            consectetur. Quasi suscipit nam cum quo quia vero!
+          </span>
         </div>
       </section>
     </div>
-    // <section>
-    //   <div className="single-pro-image">
-    //         <img src={data.urlmain} alt="" id="MainImg" width="100%"/>
-    //         <div className="small-img-group">
-    //             <div className="small-img-col">
-    //                 <img src="./download/f1.jpg"  width="100%" className="small-img"/>
-    //             </div>
-    //             <div className="small-img-col">
-    //                 <img src="./download/f2.jpg"  width="100%" className="small-img"/>
-    //             </div>
-    //             <div className="small-img-col">
-    //                 <img src="./download/f3.jpg"  width="100%" className="small-img"/>
-    //             </div>
-    //             <div className="small-img-col">
-    //                 <img src="./download/f4.jpg"  width="100%" className="small-img"/>
-    //             </div>
-    //         </div>
-    //        </div>
-    //        <div className="single-pro-details">
-    //         <h6>Home / T-Shirt</h6>
-    //         <h4>Men's Fashion T Shirt</h4>
-    //         <h2>$139.00</h2>
-    //         <select >
-    //             <option >Select Size</option>
-    //             <option >XL</option>
-    //             <option >XXL</option>
-    //             <option >Small</option>
-    //             <option >Large</option>
-    //         </select>
-    //         <input type="number" />
-    //         <button className="normal">Add To Cart</button>
-    //         <h4>Product Details</h4>
-    //         <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta culpa impedit omnis! Commodi soluta beatae facilis omnis placeat unde, sed accusamus temporibus alias, impedit laboriosam odit veritatis voluptatem ad corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem sunt, magni eos ratione repellendus atque neque corrupti magnam, natus quis illum harum consectetur. Quasi suscipit nam cum quo quia vero!</span>
-    //        </div>
-    // </section>
-  )
-}
+  );
+};
 
-export default Spage
+export default Spage;
